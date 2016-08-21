7efa02b23d2ce19993535f739db1fb9771e0b73e
kskonecka <kasia.skonecka@gmail.com>
2016-08-21 17:30:38 +0200

    js maxi image window added

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(function() {
 
   //variables for main ul list
   var productLists = $('.product-container');
-  //variables for url
+  //variable for api url
   var productUrl = 'https://www.unisport.dk/api/sample';
 
 
@@ -17,32 +17,34 @@ $(function() {
           aWrap.on('click', function (event) {
             event.preventDefault();
           })
-        var img = $('<img>', {src: product.image}).data("bla", product.img_url); //product img + data-url for maxi-img
+        var img = $('<img>', {src: product.image, class: "small-photo"}); //product img + data-url for maxi-img
         // console.log(img.data());
+        var bigPhotoIcon = $('<img>', {src:"D:/WORKSPACE/sample-grid/images/more-zoom.png", class:'big-photo-icon'}).data("big", product.img_url).addClass('hidden');
+        console.log(bigPhotoIcon.data());
         var divNameWrap = $('<div>', {class: "name-wrap"}); //div for Name
         var spanName = $('<span>').text(product.name); //span for Name
 
         var divPriceWrap = $('<div>', {class: "price-wrap"}); //div for Price
         var spanPrice = $('<span>', {class: "price"}).text(product.price); //span for Price
-        var spanCurrency = $('<span>', {class: "currency"}).text(product.currency); //currency for Price
+        var supCurrency = $('<sup>', {class: "currency"}).text(" " + product.currency); //currency for Price
         var pOldPrice = $('<p>', {class: "old-price"}); //old Price
-          if ((product.price_old) != "0,00") { //only display when the product indeed had old price
+          if (product.price_old != "0,00") { //only display when the product indeed had old price
             pOldPrice.text("Before: " + product.price_old);
           } else {
             pOldPrice.text('');
           }
-        var divDeliveryWrap = $('<div>', {class: "delivery-wrap"}); //div for Delivery
+        var divDeliveryWrap = $('<div>', {class: "delivery-wrap"}).addClass("hidden"); //div for Delivery
         var spanDelivery = $('<span>').text("Delivery: " + product.delivery); //span for Delivery
-        var divSizeWrap = $('<div>', {class: "size-wrap"}); //div for size-wrap
+        var formSizeWrap = $('<form>', {class: "size-wrap"}).addClass('hidden'); //form for size-wrap
         var spanSizeLabel = $('<span>').text("Sizes: "); //span for Size-Label
-        var spanSizes = $('<span>').text(product.sizes); //span for Sizes
+        var spanSizes = $('<span>', {class: "sizes"}).text(product.sizes); //span for Sizes
         //rest of data
         var divOtherWrap = $('<div>', {class: "other-wrap"}); //div for other-wrap
         var pInStock = $('<p>', {class: "in-stock"}); //Online (does it mean in Stock?)
           if (product.online === "1") { //text for displaying when product is in stock
             pInStock.text("In Stock");
           } else {
-            pInStock.text("Sold Out");
+            pInStock.text(" ");
           }
         var pPackage = $('<p>', {class: "package"}); //pPackage ?????????
           if (product.package === "1") {
@@ -50,34 +52,43 @@ $(function() {
           } else {
             pPackage.text("Package: No");
           }
-        var pPorto = $('<p>', {class: "porto"});
+        var pPorto = $('<p>', {class: "porto"}); //shipping??
           if (product.free_porto === "1") {
             pPorto.text("Free Shipping: Yes");
           } else {
             pPorto.text("Free Shipping: No");
           }
-        var pIdNumber = $('<p>', {class: "id-number"}).text("Product No: ");
+        var pIdNumber = $('<p>', {class: "id-number"}).text("Product No: "); //ID
         var spanIdNumber = $('<span>').text(product.id);
+
+
+        //create other elements (not json data)
+        var divReadMore = $('<div>', {class: "read-more"});
+        var aReadMore = $('<a>').text("READ MORE");
 
 
       //append elements to DOM
         divProduct.append(aWrap);
         aWrap.append(img);
+        aWrap.append(bigPhotoIcon);
         //Name
         aWrap.append(divNameWrap);
         divNameWrap.append(spanName);
         //Price
         aWrap.append(divPriceWrap);
         divPriceWrap.append(spanPrice);
-        divPriceWrap.append(spanCurrency);
+        divPriceWrap.append(supCurrency);
         divPriceWrap.append(pOldPrice);
+        //read MORE
+        aWrap.append(divReadMore);
+        divReadMore.append(aReadMore);
         //Delivery
         aWrap.append(divDeliveryWrap);
         divDeliveryWrap.append(spanDelivery);
         //Sizes
-        aWrap.append(divSizeWrap);
-        divSizeWrap.append(spanSizeLabel);
-        divSizeWrap.append(spanSizes);
+        aWrap.append(formSizeWrap);
+        formSizeWrap.append(spanSizeLabel);
+        formSizeWrap.append(spanSizes);
         //other
         aWrap.append(divOtherWrap);
         divOtherWrap.append(pInStock);
@@ -87,11 +98,50 @@ $(function() {
         pIdNumber.append(spanIdNumber);
         // product to DOM
         productLists.append(divProduct);
+
+        //function for showing big photo
+        function showBigPhoto() {
+          //show and hide zoom icon when hovering on photo
+          img.each(function(i,v){
+            $(this).on("mouseover", function(){
+              $(this).next().removeClass('hidden');
+            });
+            $(this).on("mouseleave", function(){
+              $(this).next().addClass('hidden');
+            });
+          })
+
+        //show big photo
+          bigPhotoIcon.each(function(i,v){
+            $(this).on("mouseover", function(){
+              $(this).removeClass('hidden');
+            });
+            $(this).click(function(){
+              var imageSource = $(this).data('big');
+
+              var bigImg = $('<img>'); //create big image
+              var fullScreen = $('<div>'); // create div that contains big image
+              var closeButton = $('<div>'); //create button that exits full screen
+
+              fullScreen.addClass('fullScreen').fadeIn('slow').appendTo($('.photomax-box'));//add fullScreen class and attach to <body>
+              bigImg.attr('src', imageSource).appendTo(fullScreen);//add img src and attach to <fullScreen>
+              closeButton.addClass('closeButton').appendTo(fullScreen);//add closeButton and attach to <fullScreen>
+
+              $('body').on('click', '.closeButton', function(){ //exits full screen
+                fullScreen.fadeOut('slow');
+              });
+            })
+          //hide zoom icon
+            $(this).on("mouseleave", function(){
+              $(this).addClass('hidden');
+            });
+          })
+        }
+        showBigPhoto();
     });
   }
 
-  /* Load products and insert them into the DOM
-  */
+  //Load products and insert them into the DOM
   function loadProducts() {
         $.ajax({
             	url: productUrl
