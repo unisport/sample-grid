60b3ad30762d7c015e7b3faa6695c72b53cd5b17
Kristian Nissen <kristian.nissen@gmail.com>
2020-09-24 22:12:43 +0200

    products loaded async

--- src/app.js
+++ src/app.js
@@ -1,16 +1,40 @@
 /**
  *
  */
-import React, {Suspense, lazy} from "react";
+import React, {Suspense, lazy, useState, useEffect} from "react";
 
 // Dynamic loading
 const Hello = lazy(() => import("./lib/Hello"));
 
+// Load data
+// TODO: Move to separate file
+const useFetch = url => {
+    const [data, setData] = useState(null);
+
+    async function fetchData() {
+        const response = await fetch(url);
+        const json = await response.json();
+        setData(json);
+    }
+
+    useEffect(() => {fetchData()}, [url]);
+
+    return data;
+}
+
 // Main app
 const App = () => {
-    return <Suspense fallback={<div>Loading...</div>}>
-            <Hello />
-        </Suspense>
+    const data = useFetch("/data.json");
+
+    if (data === null) {
+        // No products have been loaded
+        return <div>Loading... slow internet is a bitch!</div>;
+    } else {
+        // Products have been loaded
+        return <Suspense fallback={<div>Loading...</div>}>
+                <Hello />
+            </Suspense>
+    }
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env", "@babel/react"],
+            plugins: ["@babel/plugin-transform-runtime"],
           }
         }
       }
