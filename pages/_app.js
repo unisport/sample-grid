ea98d69c498a73ac2bdd46c0dad501c0f247f35d
Mikkel Petersen <mikkel@mikkelpetersen.dk>
2021-10-10 23:06:47 +0200

    Added Chakra UI 🎨

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default App;
