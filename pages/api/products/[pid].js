7409978bf232222b1140c6a85b7fda0d3922caab
Mikkel Petersen <mikkel@mikkelpetersen.dk>
2021-10-10 23:45:30 +0200

    Added API Middleware.

---
+++ pages/api/products/[pid].js
@@ -0,0 +1,37 @@
+/**
+ * We use Next.js API as an API Middleware. This ensures we return the correct JSON structure and only the required data.
+ * See: https://nextjs.org/docs/api-routes/introduction
+ * @example /api/products/200776,200777
+ */
+export default async (req, res) => {
+  const { pid } = req.query;
+
+  /**
+   * We ensure that the request method is GET. If not we return a 405 HTTP Status Code.
+   * See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+   */
+  if (req.method === "GET") {
+    /** We use fetch() to query the API. */
+    fetch(process.env.API_ENDPOINT + pid)
+      .then((response) => {
+        if (!response.ok) {
+          res.status(400).end();
+          throw new Error("Fatal Error: Network Response Code was not OK.");
+        }
+        return response.json();
+      })
+      .then((result) => {
+        /** We manipulate the result here using map() to create a new Array only containing the data we need. */
+        const data = result.products.map((product) => ({
+          name: product.name,
+        }));
+        res.status(200).json(data);
+      })
+      .catch((error) => {
+        res.status(400).end();
+        console.error("Fatal Error: The fetch() operation has failed: ", error);
+      });
+  } else {
+    res.status(405).end();
+  }
+};
