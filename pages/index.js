0a0477ae07d7a16336039bb5265c09f89f23172d
Mikkel Petersen <mikkel@mikkelpetersen.dk>
2021-10-12 11:34:13 +0200

    Updated Index.js.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
-const Index = () => {
+import Layout from "@/components/Layout";
+import Products from "@/components/Products";
+import { SWRConfig } from "swr";
+
+const Index = ({ fallback }) => {
   return (
-    <div className="">
-      <h1 className="">Hello World.</h1>
-    </div>
+    <Layout>
+      <SWRConfig value={{ fallback }}>
+        <Products />
+      </SWRConfig>
+    </Layout>
   );
 };
 
 export default Index;
+
+/**
+ * We use getServerSideProps to pre-fetch the data from the API and use it as a fallback for SWR. This ensures a SEO friendly site.
+ * See: https://swr.vercel.app/docs/with-nextjs
+ */
+export const getServerSideProps = async () => {
+  const results = await fetch(process.env.SITE_URL + "/api/products/");
+  const data = await results.json();
+
+  return {
+    props: {
+      fallback: {
+        "/api/products": data,
+      },
+    },
+  };
+};
