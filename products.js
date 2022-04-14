38c72bf33b8bd7529414d2733fb59a8d9531c151
AndreasChristensen89 <brogaard89@gmail.com>
2022-04-14 12:43:01 +0000

    Sort json file, add discount mark, add navbar

--- products.js
+++ products.js
@@ -1,3 +1,4 @@
+// get json
 fetch('products.json')
     .then(function (response) {
         return response.json();
@@ -9,16 +10,42 @@ fetch('products.json')
         console.log('error: ' + err);
     });
 
+// pass json array to function to use data in loop
 function appendData(data) {
-    var mainContainer = document.getElementById("productRow");
+
+    // function to sort json objects 
+    function GetSortOrder() {    
+        return function(a, b) {    
+            if (a.prices.discount_percentage > b.prices.discount_percentage) {    
+                return 1;    
+            } else if (a.prices.discount_percentage < b.prices.discount_percentage) {    
+                return -1;    
+            }    
+            return 0;    
+        }    
+    }    
+    // pass json array using function to sort
+    data.sort(GetSortOrder());
+    // reverse to highest discount comes first
+    data.reverse();  
+ 
+    // reference main row and add elements using bootstrap cards and additional styling
+    var mainRow = document.getElementById("productRow");
     for (var i = 0; i < data.length; i++) {
         var topDiv = document.createElement("div");
         topDiv.classList.add("col-7", "col-sm-6", "col-md-6", "col-lg-4", "px-1", "mx-auto");
-        mainContainer.appendChild(topDiv);
+        mainRow.appendChild(topDiv);
 
         var card = document.createElement("div");
         card.classList.add("card", "border", "product-height", "my-2", "box-shadow");
         topDiv.appendChild(card);
+
+        var discountMark = document.createElement("div");
+        discountMark.classList.add("label-container", "text-center");
+        discountMark.innerText = data[i].prices.discount_percentage + "%";
+        if (data[i].prices.discount_percentage > 0) {
+            card.appendChild(discountMark);
+        }
 
         var img = document.createElement("img");
         img.classList.add("card-img-top", "img-fluid", "all-products-img", "mx-auto", "pt-2");
@@ -41,9 +68,9 @@ function appendData(data) {
 
         var sizes = document.createElement("p");
         sizes.classList.add("card-text", "sizes");
-        sizes.innerText = "Størrelser: ";
+        sizes.innerHTML = "<b>Størrelser: </b>";
         if (data[i].stock.length > 4) {
-            sizes.innerText = "Adskillige størrelser tilgængelige"
+            sizes.innerText = "Mange størrelser tilgængelige"
         } else {
             for (var j = 0; j < data[i].stock.length; j++) {
                 if (j == data[i].stock.length - 1) {
@@ -55,5 +82,5 @@ function appendData(data) {
         }
         cardBody.appendChild(sizes);
     }
-    
+
 }
