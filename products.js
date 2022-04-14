c7e673270fc63122728821c3369ed59f6fcd07ca
AndreasChristensen89 <brogaard89@gmail.com>
2022-04-14 10:59:19 +0000

    Add bootstrap styling, add css file

--- products.js
+++ products.js
@@ -12,13 +12,16 @@ fetch('products.json')
 function appendData(data) {
     var mainContainer = document.getElementById("productRow");
     for (var i = 0; i < data.length; i++) {
+        var topDiv = document.createElement("div");
+        topDiv.classList.add("col-7", "col-sm-6", "col-md-6", "col-lg-4", "px-1", "mx-auto");
+        mainContainer.appendChild(topDiv);
+
         var card = document.createElement("div");
-        card.classList.add("card");
-        card.style.width = '18rem';
-        mainContainer.appendChild(card);
+        card.classList.add("card", "border", "product-height", "my-2", "box-shadow");
+        topDiv.appendChild(card);
 
         var img = document.createElement("img");
-        img.classList.add("card-img-top");
+        img.classList.add("card-img-top", "img-fluid", "all-products-img", "mx-auto", "pt-2");
         img.src = data[i].image;
         card.appendChild(img);
 
@@ -26,28 +29,31 @@ function appendData(data) {
         cardBody.classList.add("card-body");
         card.appendChild(cardBody);
 
-        var cardTitle = document.createElement("h5");
-        cardTitle.classList.add("card-title");
-        cardTitle.innerText = data[i].name;
-        cardBody.appendChild(cardTitle);
+        var title = document.createElement("h5");
+        title.classList.add("lead", "product-name", "mx-auto");
+        title.innerText = data[i].name;
+        cardBody.appendChild(title);
 
-        var cardParagraph = document.createElement("p");
-        cardParagraph.classList.add("card-text");
-        cardParagraph.innerText = data[i].prices.max_price + ' dkk';
-        cardBody.appendChild(cardParagraph);
+        var price = document.createElement("p");
+        price.classList.add("card-text", "price");
+        price.innerText = data[i].prices.max_price + ' dkk';
+        cardBody.appendChild(price);
 
-        // div.innerHTML = 'Name: ' + data[i].name;
-        // mainContainer.appendChild(div);
-
-        
+        var sizes = document.createElement("p");
+        sizes.classList.add("card-text", "sizes");
+        sizes.innerText = "Størrelser: ";
+        if (data[i].stock.length > 4) {
+            sizes.innerText = "Adskillige størrelser tilgængelige"
+        } else {
+            for (var j = 0; j < data[i].stock.length; j++) {
+                if (j == data[i].stock.length - 1) {
+                    sizes.innerText += data[i].stock[j].name;
+                } else {
+                    sizes.innerText += data[i].stock[j].name + ", ";
+                }
+            }
+        }
+        cardBody.appendChild(sizes);
     }
+    
 }
-
-{/* <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div> */}
