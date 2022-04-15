cee4ec10cbc3a8d6e18ef818653529f0412f63b8
AndreasChristensen89 <brogaard89@gmail.com>
2022-04-15 12:39:16 +0000

    Add additional styling and tweaks, member+discount

--- README.md
+++ README.md
@@ -16,3 +16,27 @@ https://help.github.com/articles/fork-a-repo/
 
 https://help.github.com/articles/using-pull-requests/
 
+
+Solution by Andreas Christensen
+
+My regular solution
+During my time as a student I would use Django to display items, meaning I would get the queryset from the backend in the view code, then use a template loop to display all the items. The JSON file would be imported into a datamodel that fits the data fields of the products, and I would then be able to generate queries from this. In the template I would be able to insert a filter which would use Q functionality to reload the page a generate a new queryset according to filter input.
+
+My posted solution
+In my posted solution I'm using only front-end, so I added the JSON file in the main directory. I'm doing most of the work in the Javascript file as I need to add to add the loop there, so I'm extracting the JSON contents into a data array which I can use in the loop.
+
+JS file
+The JSON data first is sorted according to discount percentage and has the object with the highest discount first.
+
+The array is then looped through, adding a card for every object. I decided to use Boostrap cards to present the products, mixed in with personal styling.
+
+Inside the card an info box is added in the top left corner which holds the potential discount tag and members only tag. These tags are added in the following.
+
+Following this a product image is added
+
+
+
+I can see in the JSON file that there is a max price, a minimum price, and a recommended retail price. The min and max are identical, so I decided to display the max price.
+
+Nexus 4 is weird
+
--- products.js
+++ products.js
@@ -26,59 +26,104 @@ function appendData(data) {
     }    
     // pass json array using function to sort
     data.sort(GetSortOrder());
-    // reverse to highest discount comes first
+    // reverse so highest discount comes first
     data.reverse();  
  
-    // reference main row and add elements using bootstrap cards and additional styling
+    // reference main row and add elements using bootstrap cards and additional personal styling
     var mainRow = document.getElementById("productRow");
     for (var i = 0; i < data.length; i++) {
         var topDiv = document.createElement("div");
-        topDiv.classList.add("col-7", "col-sm-6", "col-md-6", "col-lg-4", "px-1", "mx-auto");
+        topDiv.classList.add("col-6", "col-md-4", "col-lg-3", "px-2", "mx-auto", "hover");
         mainRow.appendChild(topDiv);
 
+        // add bootstrap card
         var card = document.createElement("div");
         card.classList.add("card", "border", "product-height", "my-2", "box-shadow");
         topDiv.appendChild(card);
 
-        var discountMark = document.createElement("div");
-        discountMark.classList.add("label-container", "text-center");
-        discountMark.innerText = data[i].prices.discount_percentage + "%";
+        // creating a container for the potential info tags
+        var infoBox = document.createElement("div");
+        infoBox.classList.add("info-container", "text-center");
+        card.appendChild(infoBox);
+
+        // if discount present then add corner div
         if (data[i].prices.discount_percentage > 0) {
-            card.appendChild(discountMark);
+            var discountMark = document.createElement("div");
+            discountMark.classList.add("discount", "mb-2");
+            discountMark.innerText = "-" + data[i].prices.discount_percentage + "%";
+            infoBox.appendChild(discountMark);
+        }
+        
+        // If members only then add the label
+        if (data[i].labels.length > 0 && data[i].labels[0].name === "Kun medlemmer") {
+            var membersOnly = document.createElement("div");
+            membersOnly.classList.add("members");
+            membersOnly.innerText = "Kun medlemmer";
+            infoBox.appendChild(membersOnly);
         }
 
+        // add product image to card
         var img = document.createElement("img");
         img.classList.add("card-img-top", "img-fluid", "all-products-img", "mx-auto", "pt-2");
         img.src = data[i].image;
         card.appendChild(img);
 
+        // add card body
         var cardBody = document.createElement("div");
         cardBody.classList.add("card-body");
         card.appendChild(cardBody);
 
+        // add title to card body
         var title = document.createElement("h5");
         title.classList.add("lead", "product-name", "mx-auto");
-        title.innerText = data[i].name;
+        // set max title string length to 60 chars
+        var titleShortened = data[i].name.slice(0, 40) + (data[i].name.length > 40 ? "..." : "");
+        title.innerText = titleShortened;
         cardBody.appendChild(title);
 
-        var price = document.createElement("p");
-        price.classList.add("card-text", "price");
-        price.innerText = data[i].prices.max_price + ' dkk';
-        cardBody.appendChild(price);
+        // create price container
+        var priceContainer = document.createElement("div");
+        priceContainer.classList.add("price-container");
+        cardBody.appendChild(priceContainer);
 
+        // create price, will be added after logic underneath
+        var price = document.createElement("div");
+        price.classList.add("card-text", "price");
+        
+        // if discount present then old price is crossed out and shown underneath, updated price shown
+        if (data[i].prices.discount_percentage > 0) {
+            // calculating discount and rounding down to two decimals
+            let calculatedNewPrice = data[i].prices.max_price*(1-(data[i].prices.discount_percentage/100));
+            let NewPriceRoundedDown = Math.floor(calculatedNewPrice);
+            price.innerHTML = NewPriceRoundedDown + ' kr';
+            priceContainer.appendChild(price);
+
+            // adding old price crossed out underneath
+            var oldPrice = document.createElement("div");
+            oldPrice.classList.add("card-text", "old-price", "text-muted", "pb-2");
+            oldPrice.innerHTML = "<s>" + data[i].prices.max_price +  " kr</s>";
+            priceContainer.appendChild(oldPrice);
+        } else {
+            // regular price added if no discount
+            price.innerText = Math.floor(data[i].prices.max_price) + ' kr';
+            price.classList.add("pb-2");
+            priceContainer.appendChild(price);
+        }
+
+        // Sizes added, but if end string longer than 60 chars it wil be replaced with standard sentence
         var sizes = document.createElement("p");
         sizes.classList.add("card-text", "sizes");
         sizes.innerHTML = "<b>Størrelser: </b>";
-        if (data[i].stock.length > 4) {
-            sizes.innerText = "Mange størrelser tilgængelige"
-        } else {
-            for (var j = 0; j < data[i].stock.length; j++) {
-                if (j == data[i].stock.length - 1) {
-                    sizes.innerText += data[i].stock[j].name;
-                } else {
-                    sizes.innerText += data[i].stock[j].name + ", ";
-                }
+        for (var j = 0; j < data[i].stock.length; j++) {
+            if (j == data[i].stock.length - 1) {
+                sizes.innerHTML += data[i].stock[j].name;
+            } else {
+                sizes.innerHTML += data[i].stock[j].name + ", ";
             }
+        }
+
+        if (sizes.innerText.length > 45) {
+            sizes.innerText = "Mange størrelser tilgængelige"
         }
         cardBody.appendChild(sizes);
     }
